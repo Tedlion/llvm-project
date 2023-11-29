942a8716e5b2882630fe1adf25155b815e7882ae
wenyu.tang <wenyu.tang@jsnebula.com>
2023-11-29 20:18:27 +0800

    temp commit. update README.md and add some cmd options.

---
+++ clang-tools-extra/class-wrapper/ClassWrapper.cpp
@@ -0,0 +1,14 @@
+/**
+ * @brief
+ * @authors tangwy
+ * @date 2024/3/27
+ */
+
+#include "ClassWrapper.h"
+
+namespace clang::class_wrapper {
+  void runClassWrapper() {
+
+  }
+
+} // namespace clang::class_wrapper
